cca74d79d43979a20c6aabbdc7ad750446077af6
Zeonx9 <mushtukov.artem@mail.ru>
2022-04-07 20:30:41 +0300

    random color of figures, collecting row and game over added

--- resid.asm
+++ resid.asm
@@ -3,6 +3,7 @@ resident segment
 	org 100h
 main: ; resident part
 jmp boot
+
 ; variables of resident part
 number2fh 	db 0D7h
 previous2fh dd ?
@@ -15,19 +16,106 @@ left 	equ 1
 right 	equ 20
 top 	equ 1
 bottom 	equ 20
+empty 	equ ' '
+flen 	equ 20
 
-field		db 0c9h, 20 dup(0cdh), 0bbh, 56 dup(' '), 13, 10 ; field is 20 x 20, however its actual size is 24 x 22 
-			db 20 dup(0bah, 20 dup(0b0h), 0bah, 56 dup(' '), 13, 10)
-			db 0c8h, 20 dup(0cdh), 0bch, 56 dup(' '), 13, 10, '$'
+field		db        0c9h, flen dup(0cdh),  0bbh, 10 dup(empty), "score:", 40 dup(empty), 13, 10 ; field is 20 x 20, however its actual size is 24 x 22 
+;			db 	      0bah, flen dup(empty), 0bah, 10 dup(empty), "000000", 40 dup(empty), 13, 10
+			db 20 dup(0bah, flen dup(empty), 0bah, 56 dup(empty), 13, 10)
+			db 		  0c8h, flen dup(0cdh),  0bch, 56 dup(empty), 13, 10, '$'
 
 cor_x		dw 1 	; [1; 20]
 cor_y		dw 1 	; [1; 20]
 old_x		dw 1 	; [1; 20]
 old_y		dw 1 	; [1; 20]
 
-play_flag 	db 1
-paused 		db 0
+play_flag 	db 0
+go_flag 	db 0
+; again_flag 	db 0
+paused 		db 1
 cursor 		dw ?
+
+colours		db 0b0h, 0b1h, 0b2h, 0dbh
+color 		db 0b0h
+next 		db ?
+
+score 		dw 0
+score_y 	dw 1
+gameover 	db "game is over!"
+go_len 		dw $-gameover
+; --------- prodedures and macroses for resident part ---------
+
+; uses ax, bx, dx, doesn't save
+get_index macro x_, y_ 
+	mov ax, y_
+	mov dl, 80
+	mul dl 
+	add ax, x_
+	mov bx, ax 
+endm
+
+; uses ax, bx, dest - db
+in_range macro dest, num, min, max
+	xor ah, ah
+	mov al, num
+	mov bl, max
+	sub bl, min ; length of range
+	div bl
+	add ah, min ; shift range
+	mov dest, ah
+endm
+
+; uses ax, bx, [color, colours, next] variables 
+set_color proc
+	call rand_next
+	in_range color, next, 0, 4
+	mov al, color
+	lea bx, colours
+	xlatb
+	mov color, al
+	ret
+set_color endp 
+
+; uses ax, bx, doesn't save, next variable
+rand_next proc
+	xor ah, ah ; formula n + 1 = (n * a + c) % m
+	mov al, next
+	mov bl, a_
+	mul bl
+	add al, c_
+	mov bl, m_
+	div bl
+	mov next, ah
+	ret
+		a_ db 17
+		c_ db 31
+		m_ db 251
+rand_next endp
+
+; uses ax, cx, dx, doesn't save next variable
+rand_seed proc
+	mov ah, 0h
+	int 1ah ; get system time (int cx:dx)
+	mov next, dl
+	ret
+rand_seed endp 
+
+print_score proc
+	get_index 37, score_y
+	mov dl, 10
+	mov ax, score 
+	digit_dec:
+		div dl 
+		add ah, '0'
+		mov field[bx], ah
+		dec bx
+		xor ah, ah 
+	or al, al 
+	jnz digit_dec
+	ret 
+print_score endp
+
+; --------- interrunt handlers ---------
 
 handler2fh proc far ; multiplex interrupt handler to interact with tsr
 	cmp ah, cs:number2fh
@@ -113,55 +201,99 @@ handler09h proc far ; keyboard interrupt handler
 		push cs 
 		pop ds 
 	push ax
+	push bx
+	push cx
+	push dx 
 		in 	al, 60h ; get scancode from 60h port
 		cmp al, 19h ; p pressed
-		jne chkfp
+		jne l9a
 			xor play_flag, 1
 			jmp not_send_code
 
-		chkfp:
-		cmp play_flag, 0
-		je pass_09h
-
-		l9w:
-		cmp al, 11h ; w pressed
-		jne l9a
-			cmp cor_y, top
-			jle not_send_code
-				dec cor_y
-				jmp not_send_code
 		l9a:
-		cmp al, 1eh ; a pressed
-		jne l9s
-			cmp cor_x, left
-			jle not_send_code
-				dec cor_x
-				jmp not_send_code
-		l9s:
-		cmp al, 1fh ; s pressed
-		jne l9x
-			cmp cor_y, bottom
-			jge not_send_code
-				inc cor_y
-				jmp not_send_code
-		l9x:
-		cmp al, 2dh ; x pressed
+		; cmp al, 1eh ; a pressed (start again)
+		; jne chkfp
+		; 	cmp go_flag, 0
+		; 	je out9a
+		; 		inc again_flag
+		; 		dec go_flag
+
+		; 	out9a: jmp not_send_code
+
+		; chkfp:
+		cmp play_flag, 1
+		je l9j 
+			jmp pass_09h
+
+		l9j:
+		cmp al, 24h ; j pressed (rotate left)
 		jne l9d
-			mov cor_y, bottom
+			; todo
 			jmp not_send_code
+
 		l9d:
-		cmp al, 20h ; d pressed
+		cmp al, 20h ; d pressed (move left)
+		jne l9k
+			cmp cor_x, left
+			jle out9d
+			get_index cor_x, cor_y
+			sub bx, 1
+			cmp field[bx], empty
+			jne out9d
+				
+				dec cor_x
+				
+			out9d: jmp not_send_code
+
+		l9k:
+		cmp al, 25h ; k pressed (rotate right)
+		jne l9v
+			; todo
+			jmp not_send_code
+
+		l9v:
+		cmp al, 2fh ; v pressed (move down)
+		jne l9f
+			cmp cor_y, bottom
+			je out9v
+
+			mov cx, bottom 
+			sub cx, cor_y
+			get_index cor_x, cor_y
+
+			check_stop:
+				cmp field[bx + 80], empty
+				jne set_point
+				add bx, 80
+			loop check_stop
+			set_point:
+
+			mov cor_y, bottom
+			sub cor_y, cx
+
+			out9v: jmp not_send_code
+
+		l9f:
+		cmp al, 21h ; f pressed (move right)
 		jne chkfp2
 			cmp cor_x, right
-			jge not_send_code
+			jge out9f
+			get_index cor_x, cor_y
+			cmp field[bx + 1], empty
+			jne out9f
+
 				inc cor_x
-				jmp not_send_code
+				
+			out9f: jmp not_send_code
 
 	chkfp2:
 	cmp play_flag, 1
 	je not_send_code
 
-	pass_09h:	
+	pass_09h:
+		pop dx 
+		pop cx
+		pop bx	
 		pop ax
 		pop ds
 		jmp dword ptr cs:previous09h
@@ -169,25 +301,25 @@ handler09h proc far ; keyboard interrupt handler
 	not_send_code:
 		mov al, 20h ; send end of interrupt code to 20h code
 		out 20h, al
+		pop dx 
+		pop cx
+		pop bx
 		pop ax
 		pop ds
-		iret ; return 
-handler09h endp
-
-get_index macro x_, y_ 
-	mov ax, y_
-	mov dl, 80
-	mul dl 
-	add ax, x_
-	mov bx, ax 
-endm 
+		iret ; return
+handler09h endp 
 
 handler1ch proc far ; timer interrupt handler
-	inc tics
-	cmp tics, 5
+	inc cs:tics
+	cmp cs:tics, 2
 	je cont_inter
 		jmp pass_1ch
 	cont_inter:
+
+	cmp cs:go_flag, 0
+	je cont_inter2
+		jmp pass_1ch
+	cont_inter2:
 
 	push ds 
 		push cs 
@@ -195,42 +327,114 @@ handler1ch proc far ; timer interrupt handler
 	push ax
 	push dx
 	push bx
+	push cx
+		; cmp again_flag, 1
+		; jne cont_pchk
+
+		; 	mov score, 0
+		; 	mov cor_y, 1
+		; 	inc score_y
+		; 	inc play_flag
+
+		; 	mov cx, flen
+		; 	get_index 1, 1
+		; 	clr_row_screen:
+		; 		push cx 
+		; 		mov cx, flen
+		; 		clr_cell_screen:
+		; 			mov field[bx], empty
+		; 			inc bx
+		; 		loop clr_cell_screen
+		; 		sub bx, flen
+		; 		add bx, 80 
+		; 		pop cx
+		; 	loop clr_row_screen
+
+		; cont_pchk:
 
 		cmp play_flag, 0
 		jne cont_play 
-			cmp paused, 0
-			je pause_game ; skip if paused else pause
-				jmp pass_draw
-			pause_game:
+		cmp paused, 0
+		je pause_game ; skip if paused else pause
+			jmp pass_draw
+		pause_game:
 				get_index old_x, old_y
 				mov field[bx], 'X' ; put signal of paused
 				inc paused
 				jmp draw_field
 		cont_play:
 
-		cmp play_flag, 1
-		jne cont_play2
-			cmp paused, 1
-			jne cont_play2 ; resume 
-				mov paused, 0
+		cmp play_flag, 0
+		je cont_play2
+		cmp paused, 0
+		je cont_play2 ; resume 
+			dec paused
 		cont_play2:
 
 			get_index old_x, old_y
-			mov field[bx], 0b0h ; erase previous
+			mov field[bx], empty ; erase previous
 
 			get_index cor_x, cor_y
-			mov field[bx], 0b2h ; set current position
+			mov al, color
+			mov field[bx], al ; set current position
 
-		cmp cor_y, bottom
+		cmp cor_y, bottom ; check if figure is at the bottom 
 		je save_and_next
 
 		get_index cor_x, cor_y 
-		cmp field[bx + 80], 0b0h
+		cmp field[bx + 80], empty ;check for figure bellow
 		jne save_and_next
 		jmp continue_fall
 
 		save_and_next:
+			cmp cor_y, 1
+			jne cont_1csave1
+				inc go_flag ; set game over flag
+				dec play_flag
+				get_index 5, 11
+				lea di, field[bx]
+				lea si, gameover 
+				mov cx, go_len
+				cpy_go:				; print gameover msg in the middle of field
+					mov al, [si]
+					mov [di], al 
+					inc si 
+					inc di 
+				loop cpy_go
+				jmp draw_field
+			cont_1csave1:
+
+			add score, 5
+			mov cx, flen
+			get_index 1, cor_y
+			chk_row:				; check if row is done
+				cmp field[bx], empty
+				je cont_1csave
+				inc bx
+			loop chk_row 
+
+			add score, 50
+			mov cx, flen
+			sub bx, flen
+			clr_row: 		; clear the row and move everything down
+				push bx 
+				move_clmn:
+					cmp field[bx], empty
+					je next_clmn
+					sub bx, 80
+					mov al, field[bx]
+					mov field[bx + 80], al
+				jmp move_clmn
+
+				next_clmn:
+				pop bx
+				inc bx
+			loop clr_row
+
+			cont_1csave:
 			mov cor_y, 1 ; put new up
+			call set_color
+			call print_score
 			jmp draw_field
 
 		continue_fall:
@@ -264,18 +468,21 @@ handler1ch proc far ; timer interrupt handler
 
 		pass_draw:
 
+	pop cx
 	pop bx
 	pop dx
 	pop ax
 	pop ds
 
-	mov tics, 0
+	mov cs:tics, 0
 
 	pass_1ch:
 	jmp dword ptr cs:previous1ch
 handler1ch endp
 
 end_of_resident:
+
+; --------- non-resident part to install and uninstall program ---------
 
 ; boot macro
 print_str macro str
@@ -350,7 +557,10 @@ try_to_install:
 		lea dx, handler1ch
 		int 21h
 
+		call rand_seed ; initialaze random
 		print_str "Installed."
+		lea dx, instruction
+		call print
 		lea dx, end_of_resident
 		int 27h
 
@@ -379,6 +589,13 @@ already_installed:
 ; variables of boot part
 off_key 	db "/off"
 flag_off	db 0
+instruction db 13, 10, "^_^  Hi!  ^_^", 13, 10, 
+			db "Wanna play Tetris?)", 13, 10 
+			db " -> press <P> to start the game and pause/resume it later", 13, 10
+			db " -> press <D> to move left and <F> to move right", 13, 10
+			db " -> press <V> to move down immediatly", 13, 10
+			db " -> press <J> to rotate left and <K> to rotate right", 13, 10, '$'
+			db " -> press <A> to start again after the game is over", 13, 10, 13, 10, '$'
 
 ; procedures
 print proc near ; dx = offset of '$'-terminated string  
